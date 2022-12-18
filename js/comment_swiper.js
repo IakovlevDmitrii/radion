efb745352d25d2f6aa0b8938753a0c8035729808
Iakovlev Dmitrii <iakovlev.dmitrii@gmail.com>
2022-12-18 22:08:47 +0600

    footer

---
+++ js/comment_swiper.js
@@ -0,0 +1,41 @@
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    grabCursor: true,
+    centeredSlides: true,
+
+    // autoplay: {
+    //     delay: 2500,
+    //     disableOnInteraction: false,
+    // },
+    // fade cube slide
+    // spaceBetween: 30,
+    // speed: 400,
+    // centeredSlides: true,
+    // zoom: true,
+
+    // slidesOffsetBefore: 20,
+    // slidesOffsetAfter: 2,
+
+    // watchSlidesProgress: true,
+
+    // resizeObserver: true,
+    // resistance: true,
+    // autoHeight: true,
+    pagination: {
+        el: '.swiper-pagination',
+        dynamicBullets: true,
+        clickable: true,
+    },
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+            slidesPerView: 1,
+            spaceBetween: 20
+        },
+        1220: {
+            slidesPerView: 1,
+            spaceBetween: 30
+        },
+    },
+});
+// modules: [Navigation, Pagination],
